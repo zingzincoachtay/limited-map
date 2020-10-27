992be6c671bf0e28afc319c88f60f027887bf1c7
zingzincoachtay <49378571+zingzincoachtay@users.noreply.github.com>
2020-10-27 17:13:15 -0400

    EOB; JSON parser reads multiple lines; implementing EXCEL to JSON parser

--- main.js
+++ main.js
@@ -1,23 +1,60 @@
 var bingmapapijson_t = 'far';
+var errmsg = '';
 
-function parseBingMapAPIJSON() {
-  let BINGMAPAPIJSON = format_json("BINGMAPAPIJSON").resourceSets[0].resources[0];
-  let pin = BINGMAPAPIJSON.point.coordinates;
-  let far = BINGMAPAPIJSON.travelDistance;
-  document.getElementById("err").innerHTML = pin;
-  format_t('no parse');
-  //if( !pin )  format_t(far);
-  //if( !far )  format_t(pin[0]+"\t"+pin[1]);
+function parseJSONs(s) {
+  if(s.length==0)  ReadyToExport(['']);
+  var t = (/\r?\n/.test(s)) ? s.split(/\r?\n/) : [s];
+  var y = []; for (let i in t)  y.push( parseBingMapAPIJSON(t[i]) );
+  document.getElementById("gErr").innerHTML = s.length;
+  ReadyToExport(y);
+}
+function ReadyToExport(x) {
+  document.getElementById(bingmapapijson_t).value = x.join("\n");
   // Excel will always paste from the copy (JSON string) from cell before the clipboard.
   // Good idea, no good use with Excel, however.
   document.getElementById(bingmapapijson_t).select();
-  document.execCommand("copy");
+  //document.execCommand("copy");
 }
-//function parseDistance() {
-  //var far = format_json("BINGMAPAPIJSON").resourceSets[0].resources[0].travelDistance;
-  //document.getElementById("far").value = pin[0]+"\t"+pin[1];// Use text input to strip any formatting
-  //document.getElementById("far").value = far;// Use text input to strip any formatting
-//}
+function parseBingMapAPIJSON(s) {
+  if(s.length==0) return '';
+  let BINGMAPAPIJSON = unstringify(s).resourceSets[0].resources[0];
+  let pin = (typeof BINGMAPAPIJSON.point !== 'undefined') ? BINGMAPAPIJSON.point.coordinates : '';
+  let far = (typeof BINGMAPAPIJSON.travelDistance !== 'undefined') ? BINGMAPAPIJSON.travelDistance : '';
+  //next, return in list, so that both `pins` and `fars` can return and distinguishable
+  return (pin.length>0) ? pin : (far>0) ? far : '';
+}
+var makeBox = {
+  Ans : function (){
+    const y = document.createElement("INPUT");
+    y.setAttribute("type","text");
+    y.setAttribute("id","final");
+    y.setAttribute("tabindex",2);
+    y.setAttribute("defaultValue","");
+    document.body.appendChild(y);
+  },
+  NAns : function (){
+    const y = document.createElement("TEXTAREA");
+    y.setAttribute("id",bingmapapijson_t);
+    y.setAttribute("rows","10");
+    y.setAttribute("cols","20");
+    document.body.appendChild(y);
+  },
+  NPinsAns : function (){
+    const y = document.createElement("TEXTAREA");
+    y.setAttribute("id","pins");
+    y.setAttribute("rows","10");
+    y.setAttribute("cols","20");
+    document.body.appendChild(y);
+  },
+  NFarsAns : function (){
+    const y = document.createElement("TEXTAREA");
+    y.setAttribute("id","fars");
+    y.setAttribute("rows","10");
+    y.setAttribute("cols","20");
+    document.body.appendChild(y);
+  }
+};
+unstringify = (s) => JSON.parse(s);
 format_json = (db) => JSON.parse( document.getElementById(db).value );
 format_t = (t) => document.getElementById(bingmapapijson_t).value = t;
 // https://d3js.org
