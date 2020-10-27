132cdd23de8229f86813ca6fd5b2087a162e8793
zingzincoachtay <49378571+zingzincoachtay@users.noreply.github.com>
2020-10-26 23:09:08 -0400

    Trying to bug fix, but needed data to validate

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 var bingmapapijson_t = 'far';
 
-function parseCoordinates() {
-  var pin = format_json("BINGMAPAPIJSON").resourceSets[0].resources[0].point.coordinates;
-  var far = format_json("BINGMAPAPIJSON").resourceSets[0].resources[0].travelDistance;
+function parseBingMapAPIJSON() {
+  let BINGMAPAPIJSON = format_json("BINGMAPAPIJSON").resourceSets[0].resources[0];
+  let pin = BINGMAPAPIJSON.point.coordinates;
+  let far = BINGMAPAPIJSON.travelDistance;
   document.getElementById("err").innerHTML = pin;
   format_t('no parse');
   //if( !pin )  format_t(far);
   //if( !far )  format_t(pin[0]+"\t"+pin[1]);
   // Excel will always paste from the copy (JSON string) from cell before the clipboard.
   // Good idea, no good use with Excel, however.
-  document.getElementById("far").select();
+  document.getElementById(bingmapapijson_t).select();
   document.execCommand("copy");
 }
-function parseDistance() {
-  var far = format_json("BINGMAPAPIJSON").resourceSets[0].resources[0].travelDistance;
-  document.getElementById("far").value = pin[0]+"\t"+pin[1];// Use text input to strip any formatting
-  document.getElementById("far").value = far;// Use text input to strip any formatting
-}
+//function parseDistance() {
+  //var far = format_json("BINGMAPAPIJSON").resourceSets[0].resources[0].travelDistance;
+  //document.getElementById("far").value = pin[0]+"\t"+pin[1];// Use text input to strip any formatting
+  //document.getElementById("far").value = far;// Use text input to strip any formatting
+//}
 format_json = (db) => JSON.parse( document.getElementById(db).value );
 format_t = (t) => document.getElementById(bingmapapijson_t).value = t;
 // https://d3js.org
