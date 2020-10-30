e47e03eff87b0e35e099742cd2d5c11041751ea5
zingzincoachtay <49378571+zingzincoachtay@users.noreply.github.com>
2020-10-30 13:42:04 -0400

    Allow parsing with other API JSON later

--- parseJSONresponses.js
+++ parseJSONresponses.js
@@ -1,26 +1,51 @@
-var bingmapapijson_t = 'far';
+var apijson_t = 'far';
+var apijson_provider = 'BINGMAP';
+var apijson_tree = {
+  'BINGMAP' : [
+    ["point","coordinates"],
+    ["travelDistance"],
+    ["travelDuration"]
+  ]
+};
 var errmsg = '';
 
 function parseJSONs(s) {
   if(s.length==0)  ReadyToExport(['']);
   var t = (/\r?\n/.test(s)) ? s.split(/\r?\n/) : [s];
-  var y = []; for (let i in t)  y.push( parseBingMapAPIJSON(t[i]) );
+  var y = []; for (let o of t)  y.push( parseAPIJSON(o) );
   ReadyToExport(y);
 }
 function ReadyToExport(x) {
-  document.getElementById(bingmapapijson_t).value = x.join("\n");
+  document.getElementById(apijson_t).value = x.join("\n");
   // Excel will always paste from the copy (JSON string) from cell before the clipboard.
   // Good idea, no good use with Excel, however.
-  document.getElementById(bingmapapijson_t).select();
+  document.getElementById(apijson_t).select();
   //document.execCommand("copy");
+}
+function parseAPIJSON(o) {
+  if( /^BINGMAP$/.test(apijson_provider) ) return parseBingMapAPIJSON(o);
 }
 function parseBingMapAPIJSON(s) {
   if(s.length==0) return '';
   let BINGMAPAPIJSON = unstringify(s).resourceSets[0].resources[0];
-  let pin = (typeof BINGMAPAPIJSON.point !== 'undefined') ? BINGMAPAPIJSON.point.coordinates : '';
-  let far = (typeof BINGMAPAPIJSON.travelDistance !== 'undefined') ? BINGMAPAPIJSON.travelDistance : '';
+  let sap = [];
+  let woods = apijson_tree[apijson_provider];
+  woods.forEach((tree, i) => {
+    sap.push(
+      ifkeythenvalue(tree,BINGMAPAPIJSON)
+    );
+  });
+  let pastable = sap.filter((v) => v.length > 0);
   //next, return in list, so that both `pins` and `fars` can return and distinguishable
-  return (pin.length>0) ? pin : (far>0) ? far : '';
+  return pastable.join("\t");
+}
+function ifkeythenvalue(nodes,o) {
+  var depth = o;
+  for(node of nodes){
+    if( !(node in depth) ) return '';// Could not establish key-value pair
+    depth = depth[node];
+  }
+  return (Array.isArray(depth)) ? depth.join("\t") : depth.toString();
 }
 var makeBox = {
   Ans : function (){
@@ -33,9 +58,9 @@ var makeBox = {
   },
   NAns : function (){
     const y = document.createElement("TEXTAREA");
-    y.setAttribute("id",bingmapapijson_t);
+    y.setAttribute("id",apijson_t);
     y.setAttribute("rows","10");
-    y.setAttribute("cols","20");
+    y.setAttribute("cols","40");
     document.body.appendChild(y);
   },
   NPinsAns : function (){
--- plot2map.js
+++ plot2map.js
@@ -1,18 +1,46 @@
-L.Icon.Default.prototype.options.iconSize = [20,30];
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+var map,markers;
+var leafletmaplayer = 'map';
+var origins = [
+  {"c":[39.640784,-86.831823],"z":1000,"name":"Heartland Automotive"},
+  {"c":[40.363639,-86.835809],"z":999,"name":"Heartland Automotive - Lafayette"}
+];
+var CirclesinMiles = [100,200,300,500,1000], inMeters = [];
+CirclesinMiles.forEach((e) => inMeters.push(e/1.609344*1000));
 
-var inMiles = [100,200,300,500,1000], inMeters = [];
-inMiles.forEach((e) => inMeters.push(e/1.609344*1000));
-
+function LiveLong(o) {
+  Object.defineProperty(this, "mapjson", {
+    set: function(o) {
+      mapjson = o;
+    },
+    get: function() {
+      return mapjson;
+    },
+    configurable: false
+  });
+}
 function onMapClick(e){
   L.popup()
     .setLatLng(e.latlng)
     .setContent("Closest supplier is "+ e.latlng.toString())
     .openOn(map);
 }
-
+function DrawBaseMapLayer(Layer,map){
+  return Layer.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+}
+function placeOrigin(x,Layer,map){
+  return Layer.marker(x.c,{
+    "keyboard" : true,
+    "title" : x.name,
+    "zIndexOffset":x.z}).addTo(map);
+}
+function placeMarker(x){
+  return L.marker([x.Latitude,x.Longitude],{
+    "keyboard" : false,
+    "title" : x.Supplier
+  }).addTo(map);
+}
 function weatherLayer(phenom,L,map) {
   if( /^hurricane$/i.test(phenom) ) return hazardHurricane(L,map);
   if( /^hail|sleet$/i.test(phenom) ) return hazardHailSleet(L,map);
@@ -28,7 +56,7 @@ function hazardHurricane(Layer,map) {
     format: 'image/png',
     transparent: true,
     attribution: "NOAA/NOS/OCS nowCOAST, NOAA/NWS and NOAA/OAR/NSSL",
-    opacity: 0.4
+    opacity: 0.5
   }).addTo(map);
 }
 function hazardHailSleet(Layer,map) {
@@ -44,3 +72,5 @@ function Clouds(Layer,map) {
   }).addTo(map);
   */
 }
+
+OffCenter = (c,y,x) => [c[0]+y,c[1]+x];
