50aadc21541271e526e9a71e30a53014f1e31e5c
zingzincoachtay <49378571+zingzincoachtay@users.noreply.github.com>
2021-03-01 17:02:02 -0500

    concept

--- production.bat
+++ production.bat
@@ -1,6 +1,15 @@
 @ECHO OFF
 :: Quickly make the public release version of the map,
 :: instead manually substituting `<script></script>`
+
+:: Stream the `main.js` line by line
+:: If the external JS placeholders do not appear in the line, output ECHO
+:: If the placeholders do appear, TYPE `JS-File`
+SET sed=%USERPROFILE%\Documents\coreutils\bin\sed.exe -E
+SET grep=%USERPROFILE%\Documents\coreutils\bin\grep.exe -E
+
+%sed% -e '/<script src="main.js"><\/script>/'
+
 FOR /F "DELIMS=" %%P IN (index.html) DO (
   IF /I %%P == "  <script src=\".\/main.js\"><\/script>" (ECHO %%P)
 )
